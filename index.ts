56073accfbf236316ca32737e2e8df73703f093e
Your Name <stephym917@gmail.com>
2021-11-06 17:50:18 -0400

    Add emojis

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import DiscordJS, { Intents } from 'discord.js';
 import dotenv from 'dotenv';
-dotenv.config();
+import * as cowsay from 'cowsay';
 
-console.log(process.env.TOKEN);
+dotenv.config();
 
 const client = new DiscordJS.Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -14,9 +14,37 @@ client.on('ready', () => {
 
 client.on('messageCreate', (message) => {
   if (message.content === 'ping') {
-    message.reply({
-      content: 'pong',
-    });
+    message
+      .react('⏲️')
+      .then(() => console.log(`Reacted to message "${message.content}"`))
+      .catch(console.error);
+
+    message
+      .reply({
+        content: 'pong',
+      })
+      .then(() => console.log(`Reacted to message "${message.content}"`))
+      .catch(console.error);
+  }
+
+  if (message.content === 'cowsay') {
+    message
+      .react('🐄')
+      .then(() => console.log(`Reacted to message "${message.content}"`))
+      .catch(console.error);
+
+    let output: string = cowsay.say({ text: 'Whoo hoo!' });
+    output = `
+      \`\`\`
+      ${output}
+      \`\`\`
+      `;
+    message
+      .reply({
+        content: output,
+      })
+      .then(() => console.log(`Reacted to message "${message.content}"`))
+      .catch(console.error);
   }
 });
 
